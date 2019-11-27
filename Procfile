b2923a73bcba42e0afa25750f9323bb5567dbd49
Ian Kim <iankim2002@gmail.com>
2019-11-26 20:08:06 -0500

    added new procfile

--- Procfile
+++ Procfile
@@ -1,1 +1,2 @@
-web: gunicorn --worker-class eventlet -w 1 module:sapp
+web: gunicorn -k eventlet app:app
+
--- app.py
+++ app.py
@@ -1,13 +1,23 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, jsonify, request
+from flask_socketio import SocketIO 
+from flask_socketio import send, emit
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'secret!'
+
+socketio = SocketIO(app)
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
-@app.route('/update_num')
-
+@app.route('/update_nums')
+def update_nums():
+    totnum = request.args.get('totnum')
+    linenum = request.args.get('linenum')
+    json = {'totnum': totnum, 'linenum': linenum} 
+    emit('update', json, namespace="/test",broadcast=True) 
+    return "true"
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    socketio.run(app)
